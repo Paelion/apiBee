06292e8030d21efb066334de56d5e4e2c23ba50a
Paelion <aelion.pauline1@gmail.com>
2019-12-06 16:26:21 +0100

    add all

---
+++ header.php
@@ -0,0 +1,40 @@
+<!-- Header -->
+<header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Bee</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#lifestyle">Lifestyle</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#hive">Rûche</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#queen">Reine</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#cause">Causes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#gallery">Vidéos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#pollution">Air</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#weather">Météo</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-warning my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</header>
